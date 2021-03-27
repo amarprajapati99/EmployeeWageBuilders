ba0bcd3194ffba6c932f3cdf45a21f2ed7b03625
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-27 15:09:23 +0530

    Add part time employee wage

--- src/com/bridelabz/EmployeeStatus.java
+++ src/com/bridelabz/EmployeeStatus.java
@@ -5,14 +5,18 @@ public class EmployeeStatus {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome employee wage builder program");
-		int isFullTime = 1;
+		
+		int isPartTime=1;
+		int isFullTime = 2;
 		
 		int empRatePerHrs = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == isFullTime) 
 			empHrs = 8;
+		else if(empCheck==isPartTime)
+			empHrs = 4;
 		 else 
 			empHrs = 0;
 			empWage = empHrs * empRatePerHrs;

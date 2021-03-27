c00a85fdcd198b53da7258b1a1574d1d2a928a84
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-27 15:54:49 +0530

    Total number of working days or total working hour

--- src/com/bridelabz/EmployeeStatus.java
+++ src/com/bridelabz/EmployeeStatus.java
@@ -5,15 +5,17 @@ public class EmployeeStatus {
 	public static final int isPartTime = 1;
 	public static final int isFullTime = 2;
 	public static final int empRatePerHrs = 20;
+	public static final int maxHrInMonth = 10;
 
-	public static final int No_Of_Working_Days = 2;
+	public static final int noOfWorkingDays = 2;
 
 	public static void main(String[] args) {
-		int empWage = 0;
+		
 		int empHrs = 0;
-		int totalEmpWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
 
-		for (int days = 0; days < No_Of_Working_Days; days++) {
+		while (totalEmpHrs <= maxHrInMonth && totalWorkingDays <= noOfWorkingDays){
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case isPartTime:
@@ -25,11 +27,12 @@ public class EmployeeStatus {
 			default:
 				empHrs = 0;
 			}
-			empWage = empHrs * empRatePerHrs;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage" + " " + empWage);
+			
+			totalEmpHrs += empHrs;
+			System.out.println("Day : " + " " + totalWorkingDays + "Emphrs: " + empHrs);
 		}
-		System.out.println("totalEmpWage" + " " + totalEmpWage);
+		int totalEmpWage = totalEmpHrs * empRatePerHrs;
+		System.out.println("total Emp Wage" + " " + totalEmpWage);
 
 	}
 }
